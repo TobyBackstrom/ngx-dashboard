65ab9dcaaef4e391aca8e42fe84df619fa91997f
Toby Backstrom <torbjorn.backstrom@teknikfabriken.se>
2025-08-10 19:16:27 +0200

    fix(widget): render full placeholder clock face and match live analog design
    - inline stroke attributes on face/ticks to ensure <use> clones render without CSS
    - replace deprecated xlink:href with href for SVG elements
    - remove outer circle from placeholder to match live analog clock

--- projects/ngx-dashboard-widgets/src/lib/clock-widget/clock-widget.metadata.ts
+++ projects/ngx-dashboard-widgets/src/lib/clock-widget/clock-widget.metadata.ts
@@ -1,32 +1,30 @@
-// clock-widget.metadata.ts
 export const svgIcon = `
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 800 800" preserveAspectRatio="xMidYMid meet">
-  <circle cx="400" cy="400" r="400" fill="transparent"/>
-  <use transform="matrix(-1,0,0,1,800,0)" xlink:href="#one-half" />
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 800" preserveAspectRatio="xMidYMid meet">
+  <use transform="matrix(-1,0,0,1,800,0)" href="#one-half" />
   <g id="one-half">
     <g id="one-fourth">
-      <path class="clock-face" d="m400 40v107" stroke-width="26.7" />
+      <path d="m400 40v107" stroke-width="26.7" stroke="currentColor" />
       <g id="one-twelfth">
         <path
-          class="clock-face"
           d="m580 88.233-42.5 73.612"
           stroke-width="26.7"
+          stroke="currentColor"
         />
         <g id="one-thirtieth">
           <path
-            class="clock-face"
             id="one-sixtieth"
             d="m437.63 41.974-3.6585 34.808"
             stroke-width="13.6"
+            stroke="currentColor"
           />
-          <use transform="rotate(6 400 400)" xlink:href="#one-sixtieth" />
+          <use transform="rotate(6 400 400)" href="#one-sixtieth" />
         </g>
-        <use transform="rotate(12 400 400)" xlink:href="#one-thirtieth" />
+        <use transform="rotate(12 400 400)" href="#one-thirtieth" />
       </g>
-      <use transform="rotate(30 400 400)" xlink:href="#one-twelfth" />
-      <use transform="rotate(60 400 400)" xlink:href="#one-twelfth" />
+      <use transform="rotate(30 400 400)" href="#one-twelfth" />
+      <use transform="rotate(60 400 400)" href="#one-twelfth" />
     </g>
-    <use transform="rotate(90 400 400)" xlink:href="#one-fourth" />
+    <use transform="rotate(90 400 400)" href="#one-fourth" />
   </g>
   <path
     class="clock-hour-hand"
